041d88cb2ab2195d41b58543dd7962a36e0d377d
ssv <saintsol@ssvs-MacBook-Air.local>
2016-11-14 12:57:25 -0500

    build pedestrian detection on MAC: add HOGSVM region proposal, integrate torch.framework and lua code

---
+++ pedestrian_detection_mac/XOR_Example/XORClassifyObject.h
@@ -0,0 +1,15 @@
+//
+//  XORClassifyObject.h
+//  XOR_Example
+//
+//  Created by Kurt Jacobs on 2015/06/15.
+//  Copyright (c) 2015 RandomDudes. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XORClassifyObject : NSObject
+
+@property (nonatomic) float* im;
+
+@end
