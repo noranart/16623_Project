6dc343a8201bddac6037143741af75f9b35c47e2
ssv <saintsol@ssvs-MacBook-Air.local>
2016-11-20 19:47:47 -0500

    change ReLU interface format

--- torch-ios/3rdparty/nn/ReLU.lua
+++ torch-ios/3rdparty/nn/ReLU.lua
@@ -1,5 +1,37 @@
-local ReLU, Parent = torch.class('nn.ReLU', 'nn.Threshold')
+local ReLU, parent = torch.class('nn.ReLU','nn.Module')
 
-function ReLU:__init(p)
-    Parent.__init(self,0,0)
+function ReLU:__init()
+   parent.__init(self)
+   self.threshold = 0 --or 1e-6
+   self.val = 0
+   
+   self:reset()
+
+end
+
+function ReLU:updateOutput(input)
+   
+   	print('#### RELU in ####')
+	print(#input)
+	print('#### end RELU in ####')
+	
+   input.nn.Threshold_updateOutput(self, input)
+   
+    print('#### RELU out ####')
+	--print(#self.output)
+	print(self.output[1][1][1][1])
+	print(self.output[1][1][1][2])
+	print(self.output[1][1][1][3])
+	print(self.output[1][1][1][4])
+	print(self.output[1][1][1][5])
+
+	print('#### end RELU out ####')
+
+   
+   return self.output
+end
+
+function ReLU:updateGradInput(input, gradOutput)
+   input.nn.Threshold_updateGradInput(self, input, gradOutput)
+   return self.gradInput
 end
